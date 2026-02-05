af4e942bb205f01776c018a900f97da78536f3f0
Daniela Spencer <danielaspencer9@gmail.com>
2026-02-05 15:16:00 -0400

    Exercise 5

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>Daniela Retemyer</h2>
+<p>Hi, i am Daniela. 24 years of age and recently married. I love computer science and am aiming to become a web/app designer/developer.</p>
+@endsection
