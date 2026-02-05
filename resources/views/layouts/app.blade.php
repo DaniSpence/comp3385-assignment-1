d625994670209c5c19e98e5c4c881610521dab23
Daniela Spencer <danielaspencer9@gmail.com>
2026-02-05 15:40:32 -0400

    Exercise 6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <title>COMP3385 - Lab 1</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   </head>
   <body>
     <header>
@@ -10,6 +11,7 @@
     </header>
     <main>
       @yield('content')
+      <img src="{{ url('images/image.png') }}" alt = "Wedding photo"/>
     </main>
     <footer>
       <p>Copyright 2026 COMP338 Inc.</p>
